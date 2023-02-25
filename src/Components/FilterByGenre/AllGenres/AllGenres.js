e90f550c781b21c3e6de5ef5de73e1a14bfb9eb0
kulikovm11 <maksym.obo@gmail.com>
2023-02-25 19:43:35 +0200

    предпоследние проверки

--- src/Components/FilterByGenre/AllGenres/AllGenres.js
+++ src/Components/FilterByGenre/AllGenres/AllGenres.js
@@ -3,13 +3,21 @@ import {useEffect, useState} from "react";
 import {movieService} from "../../../services/movieService";
 import {GenreItem} from "../GenreItem/GenreItem";
 
+
 const AllGenres = () => {
 
     const [genresArr, setGenresArr] = useState([])
 
+
     useEffect(()=>{
         movieService.getAllGenres().then(({data})=>setGenresArr([...data.genres]))
     },[])
+
+    const [selectedGenre, setSelectedGenre] = useState(null)
+
+    const handleSelectGenre = (genre) => {
+        setSelectedGenre(genre)
+    }
 
 
 
@@ -19,11 +27,14 @@ const AllGenres = () => {
 
     return (
         <div className="GENRES">
-
-            {genresArr.map(genre=><GenreItem key={genre.id} genre={genre} />)}
-
+            {selectedGenre && (
+                <div>Вы выбрали жанр: <span style={{color:'plum'}}>{selectedGenre.name}</span></div>
+            )}
+            {genresArr.map((genre) => (
+                <GenreItem key={genre.id} genre={genre} onSelect={handleSelectGenre} />
+            ))}
         </div>
-    );
-};
+    )
+}
 
 export {AllGenres};
--- src/Components/FilterByGenre/GenreItem/GenreItem.js
+++ src/Components/FilterByGenre/GenreItem/GenreItem.js
@@ -2,16 +2,22 @@ import {useNavigate} from "react-router-dom";
 
 import css from './GenresItemStyle.module.css'
 
-const GenreItem = ({genre}) => {
+const GenreItem = ({genre, onSelect}) => {
 
     const { id, name } = genre
 
     const navigate = useNavigate()
 
+    const handleClick = () => {
+        onSelect(genre)
+        navigate(id.toString(), { state: genre })
+    }
+
     return (
         <div>
 
-            <div  onClick={()=>navigate(id.toString(), {state:genre})} className={css.Items}> {name} </div>
+
+            <div onClick={handleClick} className={css.Items}> {name} </div>
 
 
         </div>
